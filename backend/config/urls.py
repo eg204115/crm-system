9bcb0369f08ca22a67f0eebc08e75c0972b007b4
Nethmi Poornima <nethmipoornima2020@gmail.com>
2026-03-26 13:33:46 +0530

    fix issue in the urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,6 +7,8 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenViewBase
 )
+from rest_framework.routers import DefaultRouter
+from core.views import CompanyViewSet, ContactViewSet
 
 
 class CustomTokenView(TokenViewBase):
@@ -19,3 +21,15 @@ urlpatterns = [
     path('api/v1/token/', CustomTokenView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', CustomTokenView.as_view(), name='token_refresh'),
 ]
+
+
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+
+#     path('api/v1/', include(router.urls)),
+# ]
+router = DefaultRouter()
+router.register(r'companies', CompanyViewSet, basename='company')
+router.register(r'contacts', ContactViewSet, basename='contact')

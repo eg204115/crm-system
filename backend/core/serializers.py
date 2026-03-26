4c79485d21e128771309ec44c157f50780c13d89
Nethmi Poornima <nethmipoornima2020@gmail.com>
2026-03-26 13:35:00 +0530

    Implement Role-Based Access Control

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+from .models import Company, Contact
 
 class CustomTokenSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -12,3 +14,25 @@ class CustomTokenSerializer(TokenObtainPairSerializer):
         }
 
         return data
+    
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
+        read_only_fields = ['organization', 'created_at']
+        
+        
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+        read_only_fields = ['organization', 'created_at']
+
+    # Extra validation (good practice)
+    def validate_email(self, value):
+        company = self.initial_data.get('company')
+
+        if Contact.objects.filter(email=value, company_id=company).exists():
+            raise serializers.ValidationError("Email must be unique within this company")
+
+        return value

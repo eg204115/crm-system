bbc8fc88a77bc2c9d2762346a420e26f613097c2
Nethmi Poornima <nethmipoornima2020@gmail.com>
2026-03-26 10:53:50 +0530

    Created Organization & User model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Organization(models.Model):
+    PLAN_CHOICES = [
+        ('BASIC', 'Basic'),
+        ('PRO', 'Pro'),
+    ]
+
+    name = models.CharField(max_length=255)
+    subscription_plan = models.CharField(max_length=10, choices=PLAN_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('ADMIN', 'Admin'),
+        ('MANAGER', 'Manager'),
+        ('STAFF', 'Staff'),
+    ]
+
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name='users'
+        # null=True,
+        # blank=True
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return self.username

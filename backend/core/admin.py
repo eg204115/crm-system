7a9a07f64cd40952a61f1c2f6fba049376287d56
Nethmi Poornima <nethmipoornima2020@gmail.com>
2026-03-26 11:53:32 +0530

    enable admin management for Company and Contact models

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Organization, User
+from .models import Organization, User, Company, Contact    
 
 admin.site.register(Organization)
 admin.site.register(User)
+admin.site.register(Company)
+admin.site.register(Contact)

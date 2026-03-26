4023478f8d54993fd64f5e98b01f05e05f7bb845
Nethmi Poornima <nethmipoornima2020@gmail.com>
2026-03-26 11:48:40 +0530

    Create company and contact models

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 class Organization(models.Model):
     PLAN_CHOICES = [
@@ -33,3 +34,61 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    industry = models.CharField(max_length=255)
+    country = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='company_logos/', null=True, blank=True)
+
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name='companies'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)  # soft delete
+
+    def __str__(self):
+        return self.name
+    
+    
+class Contact(models.Model):
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+        related_name='contacts'
+    )
+
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name='contacts'
+    )
+
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    phone_validator = RegexValidator(
+        regex=r'^\d{8,15}$',
+        message="Phone must be 8-15 digits"
+    )
+    phone = models.CharField(
+        validators=[phone_validator],
+        max_length=15,
+        null=True,
+        blank=True
+    )
+
+    role = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('email', 'company')  # IMPORTANT validation
+
+    def __str__(self):
+        return self.full_name

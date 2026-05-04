ce8accf04f4648f3eb4cb577474271f69d2bb203
Nethmi Poornima <nethmipoornima2020@gmail.com>
2026-05-04 09:04:02 +0530

    Remove unnecessary files

--- backend/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Organization, User, Company, Contact    
-
-admin.site.register(Organization)
-admin.site.register(User)
-admin.site.register(Company)
-admin.site.register(Contact)

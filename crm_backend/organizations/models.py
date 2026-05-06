31cca127c57e1430c0617a4e8cc11454d45fd4c6
Nethmi Poornima <nethmipoornima2020@gmail.com>
2026-05-06 07:39:16 +0530

    add Organization model

--- crm_backend/organizations/models.py
+++ crm_backend/organizations/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+    BASIC = 'BASIC'
+    PRO = 'PRO'
+
+    SUBSCRIPTION_CHOICES = [
+        (BASIC, 'Basic'),
+        (PRO, 'Pro'),
+    ]
+
+    name = models.CharField(max_length=255)
+    subscription_plan = models.CharField(
+        max_length=10,
+        choices=SUBSCRIPTION_CHOICES,
+        default=BASIC
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
